9b4f94c3fc1b33e18cb55176acc90a7abbfa3cb5
Joel Hills <mrmerrypoop@gmail.com>
2024-07-24 08:02:14 +1000

    Added content from previous, undocumented attempt

---
+++ 6502.h
@@ -0,0 +1,237 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef signed char byte;
+typedef unsigned char u_byte;
+typedef bool bit;
+
+typedef unsigned int __useconds_t;
+typedef unsigned int uint;
+
+typedef unsigned short ushort;
+#define RESET_VEC            0xFFFC
+
+#define CARRY_POS            0
+#define ZERO_POS             1
+#define INERRUPTDISABLE_POS  2
+#define DECIMALMODE_POS      3
+#define BRKCOMMAND_POS       4
+/* Nothing */
+#define OVERFLOW_POS         6
+#define NEGATIVE_POS         7
+
+#define Z_PAGE_END           0x100
+#define STACK_END            0x200
+
+#define MAX_FUNCTION_STACK_SIZE 5
+
+typedef enum
+{
+    IMPLIED,                        // N/A
+    ACCUMULATOR,                    // A
+    IMMEDIATE,                      // #$nn
+    ABSOLUTE,                       // $nnnn
+    X_INDEXED_ABSOLUTE,             // $nnnn,X
+    Y_INDEXED_ABSOLUTE,             // $nnnn,Y
+    ABSOLUTE_INDIRECT,              // ($nnnn)
+    ZERO_PAGE,                      // $nn
+    X_INDEXED_ZERO_PAGE,            // $nn,X
+    Y_INDEXED_ZERO_PAGE,            // $nn,Y
+    X_INDEXED_ZERO_PAGE_INDIRECT,   // ($nn,X)
+    ZERO_PAGE_INDIRECT_Y_INDEXED,   // (&nn),Y
+    RELATIVE,                       // $nnnn
+    NOTHING,                        // N/A
+} AddressingModes;
+
+typedef enum ERROR_CODES
+{
+    ERR_UNSUPPORTED_ADDR_MODE,
+    ERR_NO_INPUT_PROVIDED,
+    ERR_REGISTERS_NOT_PRESENT,
+    ERR_FUNCTION_POINTER_MISALIGNED,
+    ERR_SDL_INIT_FAILED,
+    ERR_SDL_WINDOW_ASSERTION_FAILED,
+    ERR_SDL_RENDERER_ASSERTION_FAILED,
+    ERR_SDL_TEXTURE_ASSERTION_FAILED,
+    ERR_TESTING_ERROR_CODE
+} ERROR_CODES;
+
+extern char* ADDRESSING_MODES_STRING[15];
+extern char* ERROR_CODE_STRING[9];
+
+#if defined(_WIN32) || defined(_WIN64)
+
+    typedef struct
+    {
+        byte bus;
+        bit C, Z, I, D, B, V, N;
+        u_byte A, *SP;
+        byte X, Y;
+        ushort PC;
+        byte memory[MAX_MEMORY_SIZE];
+
+        byte function_stack[MAX_FUNCTION_STACK_SIZE];
+        byte FSP;
+        __useconds_t microsecondsPerCycle;
+        bool quit;
+
+        ushort last_accessed_memory_location;
+    } cpu;
+
+    typedef struct
+    {
+        void (*InstructionPointer)(AddressingModes, int cycles, cpu *, bool page_crossed_cycle_exception);
+        AddressingModes mode;
+        int cycles;
+    } InstructionInfo;
+#elif defined(__linux__)
+
+    typedef struct __attribute__((packed, aligned(4)))
+    {
+        byte SR;
+        u_byte A;
+        byte X, Y;
+        ushort PC;
+        byte* mem;
+
+        byte function_stack[MAX_FUNCTION_STACK_SIZE];
+        byte FSP;
+        __useconds_t microsecondsPerCycle;
+        bool quit;
+
+        ushort last_accessed_memory_location;
+    } cpu;
+
+
+    typedef struct __attribute__((packed, aligned(4)))
+    {
+        void (*InstructionPointer)(AddressingModes, int cycles, cpu *, bool page_crossed_cycle_exception);
+        AddressingModes mode;
+        int cycles;
+    } InstructionInfo;
+#else
+#error "Unsupported Platform"
+#endif
+
+/*
+    ------------------------------------------------------------------------------------------------------
+    Websites used:
+        https://www.princeton.edu/~mae412/HANDOUTS/Datasheets/6502.pdf
+        https://www.masswerk.at/6502/6502_instruction_set.html#SLO
+        https://www.pagetable.com/c64ref/6502/?tab=3#
+
+        And a little help from...
+
+        https://chat.openai.com
+        https://en.wikipedia.org/wiki/MOS_Technology_6502
+        https://github.com/jdah/doomenstein-3d/blob/main/src/main_wolf.c
+
+        Yes, I have been using ChatGPT, but only when i'm too lazy to figure out some more complex things,
+        namely how the instruction function pointers work, however the details of how the CPU itself works
+        has been pretty much exclusively collected from the three topmost websites.
+    ------------------------------------------------------------------------------------------------------
+*/
+
+void ADC(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Add memory to accumulator with carry
+void AND(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // "AND" memory with accumulator
+void ASL(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Shift left one bit (memory or accumulator)
+
+void BCC(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Branch on carry clear
+void BCS(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Branch on carry set
+void BEQ(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Branch on result zero
+void BIT(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Test bits in memory with accumulator
+void BMI(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Branch on result minus
+void BNE(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Branch on result not zero
+void BPL(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Branch on result plus
+void BRK(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Force break
+void BVC(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Branch on overflow clear
+void BVS(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Branch on overflow set
+
+void CLC(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Clear carry flag
+void CLD(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Clear decimal mode flag
+void CLI(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Clear Interrupt disable flag
+void CLV(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Clear overflow flag
+void CMP(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Compare memory and accumulator
+void CPX(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Compare memory and index X
+void CPY(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Compare memory and index Y
+
+void DEC(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Decrement memory by one
+void DEX(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Decrement X index by one
+void DEY(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Decrement Y index by one
+
+void EOR(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // "Exclusive Or" memory with accumulator
+
+void INC(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Increment memory by one
+void INX(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Increment X index by one
+void INY(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Increment Y index by one
+
+void JMP(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Jump to new location
+void JSR(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Jump to subroutine
+
+void LDA(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Load accumulator with memory
+void LDX(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Load Index X with memory
+void LDY(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Load Index Y with memory
+void LSR(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Shift one bit right (Memory or accumulator)
+
+void NOP(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // No operation
+
+void ORA(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // "Or" memory with accumulator
+
+void PHA(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Push accumulator on stack
+void PHP(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Push processor status on stack
+void PLA(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Pull accumulator from stack
+void PLP(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Pull processor status from stack
+
+void ROL(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Rotate one bit left (Memory or accumulator)
+void ROR(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Rotate one bit right (Memory or accumulator)
+void RTI(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Return from interrupt
+void RTS(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Return from subroutine
+
+void SBC(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Subtract memory from accumulator with borrow
+void SEC(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Set carry flag
+
+void SED(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Set decimal mode flag
+void SEI(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Set interrupt disable mode flag
+void STA(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Store accumulator in memory
+void STX(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Store index X in memory
+void STY(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Store index Y in memory
+
+void TAX(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Transfer accumulator to index X
+void TAY(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Transfer accumulator to index Y
+void TSX(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Transfer stack pointer to index X
+void TXA(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Transfer index X to accumulator
+void TXS(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Transfer index X to stack pointer
+void TYA(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Transfer index Y to accumulator
+
+// --------------------------------------------------- //
+// ----------------- Illegal Opcodes ----------------- //
+// --------------------------------------------------- //
+
+void JAM(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // For realism; Freezes the CPU
+
+void SLO(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+void ANC(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+void RLA(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+void SRE(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+
+void ALR(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+void RRA(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+void ARR(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+void SAX(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+
+void XXA(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+void SHA(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+void TAS(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+void SHY(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+
+void SHX(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+void LAX(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+void LXA(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+void LAS(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+
+void DCP(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+void SBX(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+void ISC(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+void USBC(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);
+
+void SHS(AddressingModes addressingMode, int cycles, cpu *cpu, bool page_crossed_cycle_exception);   // Transfer Accumulator "AND" Index Register X to Stack Pointer then Store Stack Pointer "AND" Hi-Byte In Memory
--- main.c
+++ main.c
@@ -1,38 +1,300 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
-#define CARRY_POS            0
-#define ZERO_POS             1
-#define INERRUPTDISABLE_POS  2
-#define DECIMALMODE_POS      3
-#define BRKCOMMAND_POS       4
-/* Nothing */
-#define OVERFLOW_POS         6
-#define NEGATIVE_POS         7
+#include "6502.h"
 
-unsigned char A = 0,
-              X = 0,
-              Y = 0,
-              SP = 0,
-              SR = 0;
+InstructionInfo instructionTable[256] = {
+    // Instructions marked with a '*' are illegal opcodes
 
-unsigned short PC = 0;
-
-unsigned char* mem;
+    // 0x00 to 0x0F
+    { *BRK, 7,    IMPLIED },
+    { *ORA, 6,    X_INDEXED_ZERO_PAGE_INDIRECT },
+    { *JAM, -1,    NOTHING },                            // *
+    { *SLO, 8,    X_INDEXED_ZERO_PAGE_INDIRECT },       // *
+    { *NOP, 3,    ZERO_PAGE },                          // *
+    { *ORA, 3,    ZERO_PAGE },
+    { *ASL, 5,    ZERO_PAGE },
+    { *SLO, 5,    ZERO_PAGE },                          // *
+    { *PHP, 3,    IMPLIED },
+    { *ORA, 2,    IMMEDIATE },
+    { *ASL, 2,    ACCUMULATOR },
+    { *ANC, 2,    IMMEDIATE },                          // *
+    { *NOP, 4,    ABSOLUTE },                           // *
+    { *ORA, 4,    ABSOLUTE },
+    { *ASL, 6,    ABSOLUTE },
+    { *SLO, 6,    ABSOLUTE },                           // *
+    // 0x10 to 0x1F
+    { *BPL, 2,    RELATIVE },
+    { *ORA, 5,    ZERO_PAGE_INDIRECT_Y_INDEXED },
+    { *JAM, -1,    NOTHING },                            // *
+    { *SLO, 8,    ZERO_PAGE_INDIRECT_Y_INDEXED },       // *
+    { *NOP, 4,    X_INDEXED_ZERO_PAGE },                // *
+    { *ORA, 4,    X_INDEXED_ZERO_PAGE },
+    { *ASL, 6,    X_INDEXED_ZERO_PAGE },
+    { *SLO, 6,    X_INDEXED_ZERO_PAGE },                // *
+    { *CLC, 2,    IMPLIED },
+    { *ORA, 4,    Y_INDEXED_ABSOLUTE },
+    { *NOP, 2,    IMPLIED },                            // *
+    { *SLO, 7,    Y_INDEXED_ABSOLUTE },                 // *
+    { *NOP, 4,    X_INDEXED_ABSOLUTE },                 // *
+    { *ORA, 4,    X_INDEXED_ABSOLUTE },
+    { *ASL, 7,    X_INDEXED_ABSOLUTE },
+    { *SLO, 7,    X_INDEXED_ABSOLUTE },                 // *
+    // 0x20 to 0x2F
+    { *JSR, 6,    ABSOLUTE },
+    { *AND, 6,    X_INDEXED_ZERO_PAGE_INDIRECT },
+    { *JAM, -1,    NOTHING },                            // *
+    { *RLA, 8,    X_INDEXED_ZERO_PAGE_INDIRECT },       // *
+    { *BIT, 3,    ZERO_PAGE },
+    { *AND, 3,    ZERO_PAGE },
+    { *ROL, 5,    ZERO_PAGE },
+    { *RLA, 5,    ZERO_PAGE },                          // *
+    { *PLP, 4,    IMPLIED },
+    { *AND, 2,    IMMEDIATE },
+    { *ROL, 2,    ACCUMULATOR },
+    { *ANC, 2,    IMMEDIATE },                          // *
+    { *BIT, 4,    ABSOLUTE },
+    { *AND, 4,    ABSOLUTE },
+    { *ROL, 6,    ABSOLUTE },
+    { *RLA, 6,    ABSOLUTE },                           // *
+    // 0x30 to 0x3F
+    { *BMI, 2,    RELATIVE },
+    { *AND, 5,    ZERO_PAGE_INDIRECT_Y_INDEXED },
+    { *JAM, -1,    NOTHING },                            // *
+    { *RLA, 8,    ZERO_PAGE_INDIRECT_Y_INDEXED },       // *
+    { *NOP, 4,    X_INDEXED_ZERO_PAGE },                // *
+    { *AND, 4,    X_INDEXED_ZERO_PAGE },
+    { *ROL, 6,    X_INDEXED_ZERO_PAGE },
+    { *RLA, 6,    X_INDEXED_ZERO_PAGE },                // *
+    { *SEC, 2,    IMPLIED },
+    { *AND, 4,    Y_INDEXED_ABSOLUTE },
+    { *NOP, 2,    IMPLIED },                            // *
+    { *RLA, 7,    Y_INDEXED_ABSOLUTE },                 // *
+    { *NOP, 4,    X_INDEXED_ABSOLUTE },                 // *
+    { *AND, 4,    X_INDEXED_ABSOLUTE },
+    { *ROL, 7,    X_INDEXED_ABSOLUTE },
+    { *RLA, 7,    X_INDEXED_ABSOLUTE },                 // *
+    // 0x40 to 0x4F
+    { *RTI, 6,    IMPLIED },
+    { *EOR, 6,    X_INDEXED_ZERO_PAGE_INDIRECT },
+    { *JAM, -1,    NOTHING },                            // *
+    { *SRE, 8,    X_INDEXED_ZERO_PAGE_INDIRECT },       // *
+    { *NOP, 3,    ZERO_PAGE },                          // *
+    { *EOR, 3,    ZERO_PAGE },
+    { *LSR, 5,    ZERO_PAGE },
+    { *SRE, 5,    ZERO_PAGE },                          // *
+    { *PHA, 3,    IMPLIED },
+    { *EOR, 2,    IMMEDIATE },
+    { *LSR, 2,    ACCUMULATOR },
+    { *ALR, 2,    IMMEDIATE },                          // *
+    { *JMP, 2,    ABSOLUTE },
+    { *EOR, 4,    ABSOLUTE },
+    { *LSR, 6,    ABSOLUTE },
+    { *SRE, 6,    ABSOLUTE },                        // *
+    // 0x50 to 0x5F
+    { *BVC, 2,    RELATIVE },
+    { *EOR, 5,    ZERO_PAGE_INDIRECT_Y_INDEXED },
+    { *JAM, -1,    NOTHING },                            // *
+    { *SRE, 8,    ZERO_PAGE_INDIRECT_Y_INDEXED },       // *
+    { *NOP, 4,    X_INDEXED_ZERO_PAGE },                // *
+    { *EOR, 4,    X_INDEXED_ZERO_PAGE },
+    { *LSR, 6,    X_INDEXED_ZERO_PAGE },
+    { *SRE, 6,    X_INDEXED_ZERO_PAGE },                // *
+    { *CLI, 2,    IMPLIED },
+    { *EOR, 4,    Y_INDEXED_ABSOLUTE },
+    { *NOP, 2,    IMPLIED },                            // *
+    { *SRE, 7,    Y_INDEXED_ABSOLUTE },                 // *
+    { *NOP, 4,    X_INDEXED_ABSOLUTE },                 // *
+    { *EOR, 4,    X_INDEXED_ABSOLUTE },
+    { *LSR, 7,    X_INDEXED_ABSOLUTE },
+    { *SRE, 7,    X_INDEXED_ABSOLUTE },                 // *
+    // 0x60 to 0x6F
+    { *RTS, 6,    IMPLIED },
+    { *ADC, 6,    X_INDEXED_ZERO_PAGE_INDIRECT },
+    { *JAM, -1,    NOTHING },                            // *
+    { *RRA, 8,    X_INDEXED_ZERO_PAGE_INDIRECT },       // *
+    { *NOP, 3,    ZERO_PAGE },                          // *
+    { *ADC, 3,    ZERO_PAGE },
+    { *ROR, 5,    ZERO_PAGE },
+    { *RRA, 5,    ZERO_PAGE },                          // *
+    { *PLA, 4,    IMPLIED },
+    { *ADC, 2,    IMMEDIATE },
+    { *ROR, 2,    ACCUMULATOR },
+    { *ARR, 2,    IMMEDIATE },                          // *
+    { *JMP, 5,    ABSOLUTE_INDIRECT },
+    { *ADC, 4,    ABSOLUTE },
+    { *ROR, 6,    ABSOLUTE },
+    { *RRA, 6,    ABSOLUTE },                           // *
+    // 0x70 to 0x7F
+    { *BVS, 2,    RELATIVE },
+    { *ADC, 5,    ZERO_PAGE_INDIRECT_Y_INDEXED },
+    { *JAM, -1,    NOTHING },                            // *
+    { *RRA, 8,    ZERO_PAGE_INDIRECT_Y_INDEXED },       // *
+    { *NOP, 4,    X_INDEXED_ZERO_PAGE },                // *
+    { *ADC, 4,    X_INDEXED_ZERO_PAGE },
+    { *ROR, 6,    X_INDEXED_ZERO_PAGE },
+    { *RRA, 6,    X_INDEXED_ZERO_PAGE },                // *
+    { *SEI, 2,    IMPLIED },
+    { *ADC, 4,    Y_INDEXED_ABSOLUTE },
+    { *NOP, 2,    IMPLIED },                            // *
+    { *RRA, 7,    Y_INDEXED_ABSOLUTE },                 // *
+    { *NOP, 4,    X_INDEXED_ABSOLUTE },                 // *
+    { *ADC, 4,    X_INDEXED_ABSOLUTE },
+    { *ROR, 7,    X_INDEXED_ABSOLUTE },
+    { *RRA, 7,    X_INDEXED_ABSOLUTE },                 // *
+    // 0x80 to 0x8F
+    { *NOP, 2,    IMMEDIATE },                          // *
+    { *STA, 6,    X_INDEXED_ZERO_PAGE_INDIRECT },
+    { *NOP, 2,    IMMEDIATE },                          // *
+    { *SAX, 6,    X_INDEXED_ZERO_PAGE_INDIRECT },       // *
+    { *STY, 3,    ZERO_PAGE },
+    { *STA, 3,    ZERO_PAGE },
+    { *STX, 3,    ZERO_PAGE },
+    { *SAX, 3,    ZERO_PAGE },                          // *
+    { *DEY, 2,    IMPLIED },
+    { *NOP, 2,    IMMEDIATE },                          // *
+    { *TXA, 2,    IMPLIED },
+    { *XXA, 2,    IMMEDIATE },                          // *
+    { *STY, 4,    ABSOLUTE },
+    { *STA, 4,    ABSOLUTE },
+    { *STX, 4,    ABSOLUTE },
+    { *SAX, 4,    ABSOLUTE },                           // *
+    // 0x90 to 0x9F
+    { *BCC, 2,    RELATIVE },
+    { *STA, 6,    ZERO_PAGE_INDIRECT_Y_INDEXED },
+    { *JAM, -1,    NOTHING },                            // *
+    { *SHA, 6,    ZERO_PAGE_INDIRECT_Y_INDEXED },       // *
+    { *STY, 4,    X_INDEXED_ZERO_PAGE },
+    { *STA, 4,    X_INDEXED_ZERO_PAGE },
+    { *STX, 4,    Y_INDEXED_ZERO_PAGE },
+    { *SAX, 4,    Y_INDEXED_ZERO_PAGE },                // *
+    { *TYA, 2,    IMPLIED },
+    { *STA, 5,    Y_INDEXED_ABSOLUTE },
+    { *TXS, 2,    IMPLIED },
+    { *TAS, 5,    Y_INDEXED_ABSOLUTE },                 // *
+    { *SHY, 5,    X_INDEXED_ABSOLUTE },                 // *
+    { *STA, 5,    X_INDEXED_ABSOLUTE },
+    { *SHX, 5,    Y_INDEXED_ABSOLUTE },                 // *
+    { *SHA, 5,    Y_INDEXED_ABSOLUTE },                 // *
+    // 0xA0 to 0xAF
+    { *LDY, 2,    IMMEDIATE },
+    { *LDA, 6,    X_INDEXED_ZERO_PAGE_INDIRECT },
+    { *LDX, 2,    IMMEDIATE },
+    { *LAX, 6,    X_INDEXED_ZERO_PAGE_INDIRECT },       // *
+    { *LDY, 3,    ZERO_PAGE },
+    { *LDA, 3,    ZERO_PAGE },
+    { *LDX, 3,    ZERO_PAGE },
+    { *LAX, 3,    ZERO_PAGE },                          // *
+    { *TAY, 2,    IMPLIED },
+    { *LDA, 2,    IMMEDIATE },
+    { *TAX, 2,    IMPLIED },
+    { *LXA, 2,    IMMEDIATE },                          // *
+    { *LDY, 4,    ABSOLUTE },
+    { *LDA, 4,    ABSOLUTE },
+    { *LDX, 4,    ABSOLUTE },
+    { *LAX, 4,    ABSOLUTE },                           // *
+    // 0xB0 to 0xBF
+    { *BCS, 2,    RELATIVE },
+    { *LDA, 5,    ZERO_PAGE_INDIRECT_Y_INDEXED },
+    { *JAM, -1,    NOTHING },                            // *
+    { *LAX, 5,    ZERO_PAGE_INDIRECT_Y_INDEXED },       // *
+    { *LDY, 4,    X_INDEXED_ZERO_PAGE },
+    { *LDA, 4,    X_INDEXED_ZERO_PAGE },
+    { *LDX, 4,    Y_INDEXED_ZERO_PAGE },
+    { *LAX, 4,    Y_INDEXED_ZERO_PAGE },                // *
+    { *CLV, 2,    IMPLIED },
+    { *LDA, 4,    Y_INDEXED_ABSOLUTE },
+    { *TSX, 2,    IMPLIED },
+    { *LAS, 4,    Y_INDEXED_ABSOLUTE },                 // *
+    { *LDY, 4,    X_INDEXED_ABSOLUTE },
+    { *LDA, 4,    X_INDEXED_ABSOLUTE },
+    { *LDX, 4,    Y_INDEXED_ABSOLUTE },
+    { *LAX, 4,    Y_INDEXED_ABSOLUTE },                 // *
+    // 0xC0 to 0xCF
+    { *CPY, 2,    IMMEDIATE },
+    { *CMP, 6,    X_INDEXED_ZERO_PAGE_INDIRECT },
+    { *NOP, 2,    IMMEDIATE },                          // *
+    { *DCP, 8,    X_INDEXED_ZERO_PAGE_INDIRECT },       // *
+    { *CPY, 3,    ZERO_PAGE },
+    { *CMP, 3,    ZERO_PAGE },
+    { *DEC, 5,    ZERO_PAGE },
+    { *DCP, 5,    ZERO_PAGE },                          // *
+    { *INY, 2,    IMPLIED },
+    { *CMP, 2,    IMMEDIATE },
+    { *DEX, 2,    IMPLIED },
+    { *SBX, 2,    IMMEDIATE },                          // *
+    { *CPY, 4,    ABSOLUTE },
+    { *CMP, 4,    ABSOLUTE },
+    { *DEC, 6,    ABSOLUTE },
+    { *DCP, 6,    ABSOLUTE },                           // *
+    // 0xD0 to 0xDF
+    { *BNE, 2,    RELATIVE },
+    { *CMP, 5,    ZERO_PAGE_INDIRECT_Y_INDEXED },
+    { *JAM, -1,    NOTHING },                            // *
+    { *DCP, 8,    ZERO_PAGE_INDIRECT_Y_INDEXED },       // *
+    { *NOP, 4,    X_INDEXED_ZERO_PAGE },                // *
+    { *CMP, 3,    ZERO_PAGE },
+    { *DEC, 6,    X_INDEXED_ZERO_PAGE },
+    { *DCP, 6,    X_INDEXED_ZERO_PAGE },                // *
+    { *CLD, 2,    IMPLIED },
+    { *CMP, 4,    Y_INDEXED_ABSOLUTE },
+    { *NOP, 2,    IMPLIED },                            // *
+    { *DCP, 7,    Y_INDEXED_ABSOLUTE },                 // *
+    { *NOP, 4,    X_INDEXED_ABSOLUTE },                 // *
+    { *CMP, 4,    X_INDEXED_ABSOLUTE },
+    { *DEC, 7,    X_INDEXED_ABSOLUTE },
+    { *DCP, 7,    X_INDEXED_ABSOLUTE },                 // *
+    // 0xE0 to 0xEF
+    { *CPX, 2,    IMMEDIATE },
+    { *SBC, 6,    X_INDEXED_ZERO_PAGE_INDIRECT },
+    { *NOP, 2,    IMMEDIATE },                          // *
+    { *ISC, 8,    X_INDEXED_ZERO_PAGE_INDIRECT },       // *
+    { *CPX, 3,    ZERO_PAGE },
+    { *SBC, 3,    ZERO_PAGE },
+    { *INC, 5,    ZERO_PAGE },
+    { *ISC, 5,    ZERO_PAGE },                          // *
+    { *INX, 2,    IMPLIED },
+    { *SBC, 2,    IMMEDIATE },
+    { *NOP, 2,    IMPLIED },
+    { *USBC,2,    IMMEDIATE },                          // *
+    { *CPX, 4,    ABSOLUTE },
+    { *SBC, 4,    ABSOLUTE },
+    { *INC, 6,    ABSOLUTE },
+    { *ISC, 6,    ABSOLUTE },                           // *
+    // 0xF0 to 0xFF
+    { *BEQ, 2,    RELATIVE },
+    { *SBC, 5,    ZERO_PAGE_INDIRECT_Y_INDEXED },
+    { *JAM, -1,    NOTHING },                            // *
+    { *ISC, 8,    ZERO_PAGE_INDIRECT_Y_INDEXED },       // *
+    { *NOP, 4,    X_INDEXED_ZERO_PAGE },                // *
+    { *SBC, 4,    X_INDEXED_ZERO_PAGE },
+    { *INC, 6,    X_INDEXED_ZERO_PAGE },
+    { *ISC, 6,    X_INDEXED_ZERO_PAGE },                // *
+    { *SED, 2,    IMPLIED },
+    { *SBC, 4,    Y_INDEXED_ABSOLUTE },
+    { *NOP, 2,    IMPLIED },                            // *
+    { *ISC, 7,    Y_INDEXED_ABSOLUTE },                 // *
+    { *NOP, 4,    X_INDEXED_ABSOLUTE },                 // *
+    { *SBC, 4,    X_INDEXED_ABSOLUTE },
+    { *INC, 7,    X_INDEXED_ABSOLUTE },
+    { *ISC, 7,    X_INDEXED_ABSOLUTE },                 // *
+};
 
 unsigned char set_bit(unsigned char d, char i) {
     return d | (1 << i);
 }
 
 int main() {
+    cpu main;
 
+    main.mem = calloc(MAX_MEMORY_SIZE, 1);
     /* Allocate memory block and registers */
     /* 6502 could access up to 65535 bytes (64K) of memory */
     /* This is not the 6510, shut the fuck up */
     mem = calloc(UINT16_MAX, 1);
     if (mem == NULL) return -1;
-
+    PC = mem[RESET_VEC];
     // TODO: Do shit
 
     free(mem);
