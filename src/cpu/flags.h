84d53bf7a1ad65c9985e01f372102d02438b54b7
BulmasArmpitSweat <151138740+BulmasArmpitSweat@users.noreply.github.com>
2024-07-31 14:56:23 +1000

    Update flags.h
    Removed constexpr definitions, because apparently C23 isn't complete enough for it to be widely supported.

--- src/cpu/flags.h
+++ src/cpu/flags.h
@@ -10,18 +10,18 @@
 #define OVERFLOW                (1 << 6)
 #define NEGATIVE                (1 << 7)
 
-constexpr static inline void set_flag(cpu* cpu, const byte_raw flag) {
+static inline void set_flag(cpu* cpu, const byte_raw flag) {
     cpu->SR |= flag;
 }
 
-constexpr static inline void reset_flag(cpu* cpu, const byte_raw flag) {
+static inline void reset_flag(cpu* cpu, const byte_raw flag) {
     cpu->SR &= ~flag;
 }
 
-constexpr static inline void toggle_flag(cpu* cpu, const byte_raw flag) {
+static inline void toggle_flag(cpu* cpu, const byte_raw flag) {
     cpu->SR ^= (1 << flag);
 }
 
-constexpr static inline unsigned char set_bit(unsigned char d, char i) {
+static inline unsigned char set_bit(unsigned char d, char i) {
     return d | (1 << i);
 }
